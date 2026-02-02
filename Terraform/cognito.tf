276e3f6bb42ca84b715156a2ebc257575ded452b
alexgiovannir <alexgiovannir@gmail.com>
2026-02-02 20:58:05 -0300

    primeiro commit

--- Terraform/cognito.tf
+++ Terraform/cognito.tf
@@ -4,6 +4,7 @@ resource "aws_cognito_user_pool" "main" {
   username_attributes      = ["email"]
   auto_verified_attributes = ["email"]
 
+
   password_policy {
     minimum_length    = 8
     require_lowercase = true
